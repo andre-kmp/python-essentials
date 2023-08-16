0cc3c501abde35399d142a7650d7aabc7b7144f6
André Klaic <andreklaic@gmail.com>
2023-08-15 22:10:38 -0300

    First commit of the script word counter, written to practice the git fluency

---
+++ word_counter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# This program returns how many words are there in a message
+msg = input("Write your message: ")
+word_count = len(msg.rsplit())
+if word_count != 0:
+	if word_count == 1: agreement = "word"
+	if word_count > 1: agreement = "words"
+	print(f"It seems that your message has {word_count} {agreement}.\n\nThank you for requesting!\n\nLeaving...")
+else:
+	print(f"There was nothing as input! Try again!")
