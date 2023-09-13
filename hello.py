33456f5f194a061497cbadb791e240eb92f9fb91
André Klaic <andreklaic@gmail.com>
2023-09-12 23:54:11 -0300

    Script written/copied during the class CREATING A PROGRAM THAT READS ENVIRONMENT VARIABLES (Criando um programa que lê variáveis de ambiente), from the course Python Essentials that I'm taking at LINUXtips. The main difference between this code and the reference code is that I couldn't use os library to get the value of LANG. Instead, I imported locale. Maybe because it is I'm working in Windows. As soon as I learn how to get LANG value using OS, I'll update it here.

--- hello.py
+++ hello.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# This program prints Hello, World!
-print("Hello, World!")
+Dependendo da língua configurada no ambiente, 
+o programa exibe a mensagem correspondente.
 
-# It also prints a message celebrating my return to the course
-print("I'm finally back to my studies!")
+Observações iniciais:
+
+O curso Python Essentials está sendo ministrado em Linux e eu
+uso Windows. Por isso, algumas adaptações precisaram ser feitas.
+Todos os meus códigos terão que rodar em Windows.
+
+Como usar:
+
+Importe a biblioteca locale
+Tenha a variável LANG devidamente configurada. Exemplo:
+    
+    import locale
+    LANG =  locale.getdefaultlocale()[0]
+
+Execução:
+
+	python3 hello.py
+	ou
+	hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "André Klaic"
+__license__ = "Unlicense"
+
+import locale
+
+current_language = locale.getdefaultlocale()[0]
+
+msg = "Hello, World!"
+
+
+if current_language == "pt_BR":
+    msg = "Olá, mundo!"
+elif current_language == "en_US":
+	msg = "Hello, World!"
+else
+    msg = ":( Please wait until we are ready for your system :("
+
+
+
+print(msg)
